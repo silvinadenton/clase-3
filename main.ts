cba16d7c4f6b498eae77d3b95748002147da1541
Silvina <124196909+silvinadenton@users.noreply.github.com>
2025-08-07 12:27:16 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Silvina")
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("54 años")
+    basic.showIcon(IconNames.Sad)
+})
